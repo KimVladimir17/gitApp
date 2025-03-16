12e4e0fad7038959706557f6a3d24f664d573a67
KimVladimir17 <vladimir.kim200017@gmail.com>
2025-03-16 18:48:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sendRequest(query) {
   const isLocal = window.location.hostname === 'localhost';
   const apiUrl = isLocal ? 'http://localhost:3002' : 'https://git-app-blue.vercel.app';
   
-  fetch(`${apiUrl}/api/search?q=${query}`, {
+  fetch(`${apiUrl}/api/server?q=${query}`, {
     method: "GET",
   })
     .then((res) => res.json())
